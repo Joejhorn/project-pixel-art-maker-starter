ba6536379dce824d57a815421af5bb386a0dfd0c
Joe Horn <josephhornmacbook@MacBook-Pro.attlocal.net>
2018-01-18 17:44:42 -0800

    working copy - need refactoring

--- designs.js
+++ designs.js
@@ -2,9 +2,51 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+var color = {
+  currentColor: "#245232"
 }
+
+$('#inputSumbit').click((event) => {
+  let width = 1,
+      height =1;
+  width = parseInt($('#input_width').val());
+  height = parseInt($('#input_height').val());
+  getTableNumbers(width, height);
+});
+// need to understand where to put these types of commands ( on window?)
+function makeClickable(){
+$('#myTable tr').on("click",(e) => {
+  var cell = $(e.target).get(0);
+  $(cell).css({background: color.currentColor});
+});
+};
+
+function makeGrid(rows, columns) {
+  var grid = '<table id = myTable>';
+  for (row =1; row <= rows; row++) {
+    grid += '<tr>';
+    for (col = 1; col <= columns; col++){
+      grid += '<td></td>';
+    }
+    grid += '</tr>';
+  }
+  grid += '</table>';
+  console.log(grid);
+  return grid;
+}
+const getTableNumbers = (rows, columns) => {
+  //need to clear out old form
+  //need to research type of button to undersand sumnit vs other ones
+  $(".gridTable").empty();
+  $( ".gridTable" ).append(makeGrid(rows, columns));
+ makeClickable();
+};
+
+$(document).ready(function(){
+
+$('#colorPicker').on('change', function(){
+    color.currentColor = $(this).val();
+    console.log(color.currentColor);
+});
+
+});
